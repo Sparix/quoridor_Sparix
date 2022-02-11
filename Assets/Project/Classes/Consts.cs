39ba57536f9897d0beaf15429f6c4050757d04d6
Sparix <54955798+Sparix@users.noreply.github.com>
2022-02-11 18:14:56 +0200

    new constant

--- Assets/Project/Classes/Consts.cs
+++ Assets/Project/Classes/Consts.cs
@@ -1,9 +1,16 @@
 ﻿namespace Project.Classes {
     public static class Consts {
         #region GAME_CONSTANTS
-
+          
+        public const int DEFAULT_FIELD_SIZE_Y = 9;  
+        public const int DEFAULT_FIELD_SIZE_X = 9; 
         public const int DEFAULT_NUM_OF_WALLS = 10;  
-
+        
+        #endregion
+            
+        #region TAGS
+        public const string GAME_MANAGER_TAG = "GameManager";
+        
         #endregion
     }
 }
