373d7f3dd4d607f2a846ed1a25800bafae96dc56
Sparix <54955798+Sparix@users.noreply.github.com>
2022-02-11 20:53:08 +0200

    add place for wall

---
+++ Assets/Project/Scripts/PlaceForWall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scripts {
+    public class PlaceForWall : MonoBehaviour {
+        [Range(0, 1f)][SerializeField] private float radius = 0.01f;
+        [SerializeField] private int y;
+        [SerializeField] private int x;
+        // [SerializeField] private GameObject horizontal;
+        // [SerializeField] private GameObject vertical;
+
+        // public GameObject GetHorizontal => horizontal;
+        // public GameObject GetVertical => vertical;
+
+        public int GetY => y;
+        public int GetX => x;
+
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.position, radius);
+        }
+    }
+}
